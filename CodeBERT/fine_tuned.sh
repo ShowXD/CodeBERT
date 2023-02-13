1a88ec945834ccbe8bc5bd5a2084bd772b882b7a
薛佾展 <n16418113@gmail.com>
2023-02-13 16:09:05 +0800

    移除指令複製檔案及自製的嘗試資料集
    1. 移除指令文本
    2. 加入執行bash檔案
    3. 移除自製資料集

---
+++ CodeBERT/fine_tuned.sh
@@ -0,0 +1,17 @@
+cd code2nl
+
+lang=python #programming language
+lr=5e-5
+batch_size=4
+beam_size=10
+source_length=256
+target_length=128
+data_dir=../data/code2nl/CodeSearchNet
+output_dir=model/$lang
+train_file=$data_dir/$lang/train.jsonl
+dev_file=$data_dir/$lang/valid.jsonl
+eval_steps=1000 #400 for ruby, 600 for javascript, 1000 for others
+train_steps=50000 #20000 for ruby, 30000 for javascript, 50000 for others
+pretrained_model=microsoft/codebert-base #Roberta: roberta-base
+
+python run.py --do_train --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --train_steps $train_steps --eval_steps $eval_steps
